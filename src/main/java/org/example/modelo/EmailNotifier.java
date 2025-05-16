e759cf4caf84ff266dc0ddb3ff1d827234d6bae4
Facundo Jonas <fjonas@uade.edu.ar>
2025-05-16 19:32:46 -0300

    Cambios realizados: Se elimino la clase de EmailNotifier.java ya que se encontraba repetida en el codigo y se comprobo el mismo para la entrega del mismo el dia 16/05 a las 7:32hs

--- src/main/java/org/example/modelo/EmailNotifier.java
+++
@@ -1,9 +0,0 @@
-package main.java.org.example.modelo;
-
-public class EmailNotifier implements Observer {
-    @Override
-    public void update(Pedido pedido, String mensaje) {
-        System.out.printf("[EMAIL a %s]: %s\n",
-                pedido.getCliente().getEmail(), mensaje);
-    }
-}
